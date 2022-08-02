2d840a7d0865d83494655e49b92ad76ea75b06af
Jack Davenport <jdavenport@vervio.com.au>
2022-08-02 11:00:57 +1000

    Added colours to output

--- aws-add-key.sh
+++ aws-add-key.sh
@@ -1,49 +1,67 @@
 #!/bin/bash
 
-echo "================== AWS ADD KEY =================="
-echo "                 Vervio Digital"
-echo ""
+# ------------------------------------ Utilities ------------------------------------
+
+# add print function capable of printing colour codes
+print() {
+    echo -e $*
+}
+
+# colours
+# source: https://stackoverflow.com/a/28938235
+Color_Off='\033[0m'
+On_Cyan='\033[46m'
+Yellow='\033[0;33m'
+UGreen='\033[4;32m'
+IRed='\033[0;91m'
+IGreen='\033[0;92m'
+
+# ------------------------------------ Start script ------------------------------------
+
+print "$On_Cyan================== AWS ADD KEY =================="
+print "$On_Cyan|----------------Vervio Digital-----------------|"
+print "$Color_Off"
 
 # make sure user isn't running as root
 if [[ $(whoami) == "root" ]]; then
-    echo "DO NOT RUN THIS SCRIPT AS ROOT!"
-    echo "Running as root will result in your SSH key being added incorrectly!"
-    echo ""
-    echo "Remove the 'sudo' from the command, or press Ctrl+D to exit your root shell if you created one"
+    print "${IRed}DO NOT RUN THIS SCRIPT AS ROOT!"
+    print "Running as root will result in your SSH key being added incorrectly!"
+    print ""
+    print "Remove the 'sudo' from the command, or press Ctrl+D to exit your root shell if you created one"
     exit 1
 fi
 
 # ask user to get public key
-echo "Please run the following command on your local terminal to get your SSH key:"
-echo ""
-echo " $ cat ~/.ssh/id_rsa.pub"
-echo ""
-echo "(if your key is in a different file read the appropriate one, make sure it's the .pub file)"
+print "${UGreen}Please run the following command on your local terminal to get your SSH key:"
+print ""
+print "$Yellow $ cat ~/.ssh/id_rsa.pub $Color_Off"
+print ""
+print "(if your key is in a different file read the appropriate one, make sure it's the .pub file)"
 read -p "Press Enter to continue..."
 
 # read public key
-echo ""
-echo "Please paste your public key into the following prompt."
-echo " (e.g. ssh-rsa AAAAB3NzaC.....DrTmjz3D username@hostname)"
-echo ""
+print ""
+print "${UGreen}Please paste your public key into the following prompt.$Color_Off"
+print " (e.g. ssh-rsa AAAAB3NzaC.....DrTmjz3D username@hostname)"
+print ""
 read -p "Enter your public key: " PUBLIC_KEY
 
 # add the SSH key to the key file
-echo "Adding your public key..."
+print "Adding your public key..."
 mkdir -p ~/.ssh && chmod go-rwx ~/.ssh
-echo $PUBLIC_KEY >> ~/.ssh/authorized_keys
+print $PUBLIC_KEY >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
-echo ""
-echo "Done!"
+print ""
+print "${IGreen}Done!"
 
 # print ssh login info
 USER=$(whoami)
 IP=$(hostname -I)
-echo ""
-echo "Your SSH key has been added! Please use the following credentials to log in:"
-echo ""
-echo " $ ssh $USER@$IP"
-echo ""
+print ""
+print "${Green}Your SSH key has been added! Please use the following credentials to log in:"
+print ""
+print "${Yellow} $ ssh $USER@$IP"
+print ""
 
 exit 0
